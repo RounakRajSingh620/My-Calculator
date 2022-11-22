d5d391fa650f935a65e43d4069aa8fd6cce46391
RounakRajSingh <raunakrajsingh620@gmail.com>
2022-11-22 23:59:36 +0530

    My Calculator

--- calc.js
+++ calc.js
@@ -1,18 +1,29 @@
-   
-  
-   
-   (function() {
-    let screen= document.querySelector('.screen');
-    let buttons= document.querySelectorAll('.cal');
-    let clear= document.querySelector('.operands_clear');
-    let equal= document.querySelector('.operands_equal');
-    
-    buttons.forEach(function(button){
-      button.addEventListener('click',function(e) {
-        let value= e.target.dataset.num;
-        screen.value += value;
-      })  
-    });
+
+
+
+(function () {
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.cal');
+  let clear = document.querySelector('.operands_clear');
+  let equal = document.querySelector('.operands_equal');
+
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    })
+  });
+  equal.addEventListener('click', function (e) {
+  if (screen.value === '') {
+      screen.value = "Please enter";
+    } else { 
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+ clear.addEventListener('click',function (e){
+    screen.value=answer;
+  })
 
 })();
 
